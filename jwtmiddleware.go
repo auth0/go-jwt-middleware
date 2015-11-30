0bf58f348de5982542ee5ff800db56a97f356afc
Matt Messinger <matthew.messinger@gmail.com>
2015-11-29 17:49:31 -0700

    add FromCookie token extractor

--- jwtmiddleware.go
+++ jwtmiddleware.go
@@ -137,6 +137,19 @@ func FromParameter(param string) TokenExtractor {
 	}
 }
 
+// FromCookie returns a function that extracts the token from the specified
+// key in the HTTP cookie, like "access_token"
+func FromCookie(accessTokenName string) TokenExtractor {
+	return func(r *http.Request) (string, error) {
+		cookie, _ := r.Cookie(accessTokenName)
+		if cookie != nil {
+			return cookie.Value, nil
+		} else {
+			return "", nil
+		}
+	}
+}
+
 // FromFirst returns a function that runs multiple token extractors and takes the
 // first token it finds
 func FromFirst(extractors ...TokenExtractor) TokenExtractor {
@@ -160,7 +173,7 @@ func (m *JWTMiddleware) CheckJWT(w http.ResponseWriter, r *http.Request) error {
 			return nil
 		}
 	}
-	
+
 	// Use the specified token extractor to extract a token from the request
 	token, err := m.Options.Extractor(r)
 
