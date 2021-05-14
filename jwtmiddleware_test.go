18a2cf680958339139b401d24d681ac38a3875b8
Jon Carl <jon.carl@auth0.com>
2021-05-14 14:52:00 -0600

    main middleware tests (#90)

--- jwtmiddleware_test.go
+++ jwtmiddleware_test.go
@@ -1,216 +1,319 @@
 package jwtmiddleware
 
 import (
-	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
-	"strings"
+	"net/url"
 	"testing"
 
-	"github.com/form3tech-oss/jwt-go"
-	"github.com/gorilla/mux"
-	. "github.com/smartystreets/goconvey/convey"
-	"github.com/urfave/negroni"
+	"github.com/google/go-cmp/cmp"
 )
 
-// defaultAuthorizationHeaderName is the default header name where the Auth
-// token should be written
-const defaultAuthorizationHeaderName = "Authorization"
+// defaults tests against the default setup
+// TODO(joncarl): replace with actual JWTs once we have the validate stuff plumbed in
+func Test_defaults(t *testing.T) {
+	tests := []struct {
+		name    string
+		options []Option
+		method  string
+		token   string
 
-// userPropertyName is the property name that will be set in the request context
-const userPropertyName = "custom-user-property"
-
-// the bytes read from the keys/sample-key file
-// private key generated with http://kjur.github.io/jsjws/tool_jwt.html
-var privateKey []byte
-
-// TestUnauthenticatedRequest will perform requests with no Authorization header
-func TestUnauthenticatedRequest(t *testing.T) {
-	Convey("Simple unauthenticated request", t, func() {
-		Convey("Unauthenticated GET to / path should return a 200 response", func() {
-			w := makeUnauthenticatedRequest("GET", "/")
-			So(w.Code, ShouldEqual, http.StatusOK)
-		})
-		Convey("Unauthenticated GET to /protected path should return a 401 response", func() {
-			w := makeUnauthenticatedRequest("GET", "/protected")
-			So(w.Code, ShouldEqual, http.StatusUnauthorized)
-		})
-	})
-}
-
-// TestAuthenticatedRequest will perform requests with an Authorization header
-func TestAuthenticatedRequest(t *testing.T) {
-	var e error
-	privateKey, e = readPrivateKey()
-	if e != nil {
-		panic(e)
-	}
-	Convey("Simple authenticated requests", t, func() {
-		Convey("Authenticated GET to / path should return a 200 response", func() {
-			w := makeAuthenticatedRequest("GET", "/", jwt.MapClaims{"foo": "bar"}, nil)
-			So(w.Code, ShouldEqual, http.StatusOK)
-		})
-		Convey("Authenticated GET to /protected path should return a 200 response if expected algorithm is not specified", func() {
-			var expectedAlgorithm jwt.SigningMethod = nil
-			w := makeAuthenticatedRequest("GET", "/protected", jwt.MapClaims{"foo": "bar"}, expectedAlgorithm)
-			So(w.Code, ShouldEqual, http.StatusOK)
-			responseBytes, err := ioutil.ReadAll(w.Body)
-			if err != nil {
-				panic(err)
-			}
-			responseString := string(responseBytes)
-			// check that the encoded data in the jwt was properly returned as json
-			So(responseString, ShouldEqual, `{"text":"bar"}`)
-		})
-		Convey("Authenticated GET to /protected path should return a 200 response if expected algorithm is correct", func() {
-			expectedAlgorithm := jwt.SigningMethodHS256
-			w := makeAuthenticatedRequest("GET", "/protected", jwt.MapClaims{"foo": "bar"}, expectedAlgorithm)
-			So(w.Code, ShouldEqual, http.StatusOK)
-			responseBytes, err := ioutil.ReadAll(w.Body)
-			if err != nil {
-				panic(err)
-			}
-			responseString := string(responseBytes)
-			// check that the encoded data in the jwt was properly returned as json
-			So(responseString, ShouldEqual, `{"text":"bar"}`)
-		})
-		Convey("Authenticated GET to /protected path should return a 401 response if algorithm is not expected one", func() {
-			expectedAlgorithm := jwt.SigningMethodRS256
-			w := makeAuthenticatedRequest("GET", "/protected", jwt.MapClaims{"foo": "bar"}, expectedAlgorithm)
-			So(w.Code, ShouldEqual, http.StatusUnauthorized)
-			responseBytes, err := ioutil.ReadAll(w.Body)
-			if err != nil {
-				panic(err)
-			}
-			responseString := string(responseBytes)
-			// check that the encoded data in the jwt was properly returned as json
-			So(strings.TrimSpace(responseString), ShouldEqual, "Expected RS256 signing method but token specified HS256")
-		})
-	})
-}
-
-func makeUnauthenticatedRequest(method string, url string) *httptest.ResponseRecorder {
-	return makeAuthenticatedRequest(method, url, nil, nil)
-}
-
-func makeAuthenticatedRequest(method string, url string, c jwt.Claims, expectedSignatureAlgorithm jwt.SigningMethod) *httptest.ResponseRecorder {
-	r, _ := http.NewRequest(method, url, nil)
-	if c != nil {
-		token := jwt.New(jwt.SigningMethodHS256)
-		token.Claims = c
-		// private key generated with http://kjur.github.io/jsjws/tool_jwt.html
-		s, e := token.SignedString(privateKey)
-		if e != nil {
-			panic(e)
-		}
-		r.Header.Set(defaultAuthorizationHeaderName, fmt.Sprintf("bearer %v", s))
-	}
-	w := httptest.NewRecorder()
-	n := createNegroniMiddleware(expectedSignatureAlgorithm)
-	n.ServeHTTP(w, r)
-	return w
-}
-
-func createNegroniMiddleware(expectedSignatureAlgorithm jwt.SigningMethod) *negroni.Negroni {
-	// create a gorilla mux router for public requests
-	publicRouter := mux.NewRouter().StrictSlash(true)
-	publicRouter.Methods("GET").
-		Path("/").
-		Name("Index").
-		Handler(http.HandlerFunc(indexHandler))
-
-	// create a gorilla mux route for protected requests
-	// the routes will be tested for jwt tokens in the default auth header
-	protectedRouter := mux.NewRouter().StrictSlash(true)
-	protectedRouter.Methods("GET").
-		Path("/protected").
-		Name("Protected").
-		Handler(http.HandlerFunc(protectedHandler))
-	// create a negroni handler for public routes
-	negPublic := negroni.New()
-	negPublic.UseHandler(publicRouter)
-
-	// negroni handler for api request
-	negProtected := negroni.New()
-	//add the JWT negroni handler
-	negProtected.Use(negroni.HandlerFunc(JWT(expectedSignatureAlgorithm).HandlerWithNext))
-	negProtected.UseHandler(protectedRouter)
-
-	//Create the main router
-	mainRouter := mux.NewRouter().StrictSlash(true)
-
-	mainRouter.Handle("/", negPublic)
-	mainRouter.Handle("/protected", negProtected)
-	//if routes match the handle prefix then I need to add this dummy matcher {_dummy:.*}
-	mainRouter.Handle("/protected/{_dummy:.*}", negProtected)
-
-	n := negroni.Classic()
-	// This are the "GLOBAL" middlewares that will be applied to every request
-	// examples are listed below:
-	//n.Use(gzip.Gzip(gzip.DefaultCompression))
-	//n.Use(negroni.HandlerFunc(SecurityMiddleware().HandlerFuncWithNext))
-	n.UseHandler(mainRouter)
-
-	return n
-}
-
-// JWT creates the middleware that parses a JWT encoded token
-func JWT(expectedSignatureAlgorithm jwt.SigningMethod) *JWTMiddleware {
-	return New(Options{
-		Debug:               false,
-		CredentialsOptional: false,
-		UserProperty:        userPropertyName,
-		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
-			if privateKey == nil {
-				var err error
-				privateKey, err = readPrivateKey()
-				if err != nil {
-					panic(err)
-				}
-			}
-			return privateKey, nil
+		wantToken      map[string]string
+		wantStatusCode int
+		wantBody       string
+	}{
+		{
+			name: "happy path",
+			options: []Option{WithValidateToken(func(token string) (interface{}, error) {
+				return map[string]string{"foo": "bar"}, nil
+			})},
+			token:          "bearer abc",
+			wantToken:      map[string]string{"foo": "bar"},
+			wantStatusCode: http.StatusOK,
+			wantBody:       "authenticated",
 		},
-		SigningMethod: expectedSignatureAlgorithm,
+		{
+			name: "validate on options",
+			options: []Option{WithValidateToken(func(token string) (interface{}, error) {
+				return map[string]string{"foo": "bar"}, nil
+			})},
+			method:         http.MethodOptions,
+			token:          "bearer abc",
+			wantToken:      map[string]string{"foo": "bar"},
+			wantStatusCode: http.StatusOK,
+			wantBody:       "authenticated",
+		},
+		{
+			name: "bad token format",
+			options: []Option{WithValidateToken(func(token string) (interface{}, error) {
+				return map[string]string{"foo": "bar"}, nil
+			})},
+			token:          "abc",
+			wantStatusCode: http.StatusInternalServerError,
+		},
+		{
+			name: "credentials not optional",
+			options: []Option{WithValidateToken(func(token string) (interface{}, error) {
+				return map[string]string{"foo": "bar"}, nil
+			})},
+			token:          "",
+			wantStatusCode: http.StatusBadRequest,
+		},
+		{
+			name: "validate token errors",
+			options: []Option{WithValidateToken(func(token string) (interface{}, error) {
+				return nil, errors.New("validate token error")
+			})},
+			token:          "bearer abc",
+			wantStatusCode: http.StatusUnauthorized,
+		},
+		{
+			name: "validateOnOptions set to false",
+			options: []Option{
+				WithValidateOnOptions(false),
+				WithValidateToken(func(token string) (interface{}, error) {
+					return nil, errors.New("should not hit me since we are not validating on options")
+				}),
+			},
+			method:         http.MethodOptions,
+			token:          "bearer abc",
+			wantStatusCode: http.StatusOK,
+			wantBody:       "authenticated",
+		},
+		{
+			name: "tokenExtractor errors",
+			options: []Option{WithTokenExtractor(func(r *http.Request) (string, error) {
+				return "", errors.New("token extractor error")
+			})},
+			wantStatusCode: http.StatusInternalServerError,
+		},
+		{
+			name: "credentialsOptional true",
+			options: []Option{
+				WithCredentialsOptional(true),
+				WithTokenExtractor(func(r *http.Request) (string, error) {
+					return "", nil
+				}),
+				WithValidateToken(func(token string) (interface{}, error) {
+					return nil, errors.New("should not hit me since credentials are optional and there are none")
+				}),
+			},
+			wantStatusCode: http.StatusOK,
+			wantBody:       "authenticated",
+		},
+		{
+			name: "credentialsOptional false",
+			options: []Option{
+				WithCredentialsOptional(false),
+				WithTokenExtractor(func(r *http.Request) (string, error) {
+					return "", nil
+				}),
+				WithValidateToken(func(token string) (interface{}, error) {
+					return nil, errors.New("should not hit me since ErrJWTMissing should be returned")
+				}),
+			},
+			wantStatusCode: http.StatusBadRequest,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			var actualContextToken map[string]string
+
+			if tc.method == "" {
+				tc.method = http.MethodGet
+			}
+
+			m := New(tc.options...)
+			ts := httptest.NewServer(m.CheckJWT(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				if ctxToken, ok := r.Context().Value(ContextKey{}).(map[string]string); ok {
+					actualContextToken = ctxToken
+				}
+				fmt.Fprint(w, "authenticated")
+			})))
+			defer ts.Close()
+
+			client := ts.Client()
+			req, _ := http.NewRequest(tc.method, ts.URL, nil)
+
+			if len(tc.token) > 0 {
+				req.Header.Add("Authorization", tc.token)
+			}
+
+			res, err := client.Do(req)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			body, err := ioutil.ReadAll(res.Body)
+			res.Body.Close()
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if want, got := tc.wantStatusCode, res.StatusCode; want != got {
+				t.Fatalf("want status code %d, got %d", want, got)
+			}
+
+			if want, got := tc.wantBody, string(body); !cmp.Equal(want, got) {
+				t.Fatal(cmp.Diff(want, got))
+			}
+
+			if want, got := tc.wantToken, actualContextToken; !cmp.Equal(want, got) {
+				t.Fatal(cmp.Diff(want, got))
+			}
+		})
+	}
+
+}
+
+func Test_invalidError(t *testing.T) {
+	t.Run("Is", func(t *testing.T) {
+		e := invalidError{details: errors.New("error details")}
+
+		if !errors.Is(&e, ErrJWTInvalid) {
+			t.Fatal("expected invalidError to be ErrJWTInvalid via errors.Is, but it was not")
+		}
+	})
+
+	t.Run("Error", func(t *testing.T) {
+		e := invalidError{details: errors.New("error details")}
+
+		mustErrorMsg(t, "jwt invalid: error details", &e)
+	})
+
+	t.Run("Unwrap", func(t *testing.T) {
+		expectedErr := errors.New("expected err")
+		e := invalidError{details: expectedErr}
+
+		// under the hood errors.Is is unwrapping the invalidError via
+		// Unwrap().
+		if !errors.Is(&e, expectedErr) {
+			t.Fatal("expected invalidError to be expectedErr via errors.Is, but it was not")
+		}
 	})
 }
 
-// readPrivateKey will load the keys/sample-key file into the
-// global privateKey variable
-func readPrivateKey() ([]byte, error) {
-	privateKey, e := ioutil.ReadFile("keys/sample-key")
-	return privateKey, e
+func Test_MultiTokenExtractor(t *testing.T) {
+	t.Run("uses first extractor that replies", func(t *testing.T) {
+		wantToken := "i am token"
+
+		exNothing := func(r *http.Request) (string, error) {
+			return "", nil
+		}
+		exSomething := func(r *http.Request) (string, error) {
+			return wantToken, nil
+		}
+		exFail := func(r *http.Request) (string, error) {
+			return "", errors.New("should not have hit me")
+		}
+
+		ex := MultiTokenExtractor(exNothing, exSomething, exFail)
+
+		gotToken, err := ex(&http.Request{})
+		mustErrorMsg(t, "", err)
+
+		if wantToken != gotToken {
+			t.Fatalf("wanted token: %q, got: %q", wantToken, gotToken)
+		}
+	})
+
+	t.Run("stops when an extractor fails", func(t *testing.T) {
+		wantErr := "extraction fail"
+
+		exNothing := func(r *http.Request) (string, error) {
+			return "", nil
+		}
+		exFail := func(r *http.Request) (string, error) {
+			return "", errors.New(wantErr)
+		}
+
+		ex := MultiTokenExtractor(exNothing, exFail)
+
+		gotToken, err := ex(&http.Request{})
+		mustErrorMsg(t, wantErr, err)
+
+		if gotToken != "" {
+			t.Fatalf("did not want a token but got: %q", gotToken)
+		}
+	})
+
+	t.Run("defaults to empty", func(t *testing.T) {
+		exNothing := func(r *http.Request) (string, error) {
+			return "", nil
+		}
+
+		ex := MultiTokenExtractor(exNothing, exNothing, exNothing)
+
+		gotToken, err := ex(&http.Request{})
+		mustErrorMsg(t, "", err)
+
+		if "" != gotToken {
+			t.Fatalf("wanted empty token but got: %q", gotToken)
+		}
+	})
 }
 
-// indexHandler will return an empty 200 OK response
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-}
+func Test_ParameterTokenExtractor(t *testing.T) {
+	wantToken := "i am token"
+	param := "i-am-param"
 
-// protectedHandler will return the content of the "foo" encoded data
-// in the token as json -> {"text":"bar"}
-func protectedHandler(w http.ResponseWriter, r *http.Request) {
-	// retrieve the token from the context
-	u := r.Context().Value(userPropertyName)
-	user := u.(*jwt.Token)
-	respondJSON(user.Claims.(jwt.MapClaims)["foo"].(string), w)
-}
+	u, err := url.Parse(fmt.Sprintf("http://localhost?%s=%s", param, wantToken))
+	mustErrorMsg(t, "", err)
+	r := &http.Request{URL: u}
 
-// Response quick n' dirty Response struct to be encoded as json
-type Response struct {
-	Text string `json:"text"`
-}
+	ex := ParameterTokenExtractor(param)
 
-// respondJSON will take an string to write through the writer as json
-func respondJSON(text string, w http.ResponseWriter) {
-	response := Response{text}
+	gotToken, err := ex(r)
+	mustErrorMsg(t, "", err)
 
-	jsonResponse, err := json.Marshal(response)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+	if wantToken != gotToken {
+		t.Fatalf("wanted token: %q, got: %q", wantToken, gotToken)
 	}
-	w.Header().Set("Content-Type", "application/json")
-	_, _ = w.Write(jsonResponse)
+}
+
+func Test_AuthHeaderTokenExtractor(t *testing.T) {
+	tests := []struct {
+		name      string
+		request   *http.Request
+		wantToken string
+		wantError string
+	}{
+		{
+			name:    "empty / no header",
+			request: &http.Request{},
+		},
+		{
+			name:      "token in header",
+			request:   &http.Request{Header: http.Header{"Authorization": []string{fmt.Sprintf("Bearer %s", "i-am-token")}}},
+			wantToken: "i-am-token",
+		},
+		{
+			name:      "no bearer",
+			request:   &http.Request{Header: http.Header{"Authorization": []string{"i-am-token"}}},
+			wantError: "Authorization header format must be Bearer {token}",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			gotToken, gotError := AuthHeaderTokenExtractor(tc.request)
+			mustErrorMsg(t, tc.wantError, gotError)
+
+			if tc.wantToken != gotToken {
+				t.Fatalf("wanted token: %q, got: %q", tc.wantToken, gotToken)
+			}
+
+		})
+	}
+}
+
+func mustErrorMsg(t testing.TB, want string, got error) {
+	if (want == "" && got != nil) ||
+		(want != "" && (got == nil || got.Error() != want)) {
+		t.Fatalf("want error: %s, got %v", want, got)
+	}
 }
