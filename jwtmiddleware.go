cceddfb42f39277466b8f1d2c9d9903616ee1bd0
Jorge L. Fatta <jorge.fatta@auth0.com>
2021-05-21 15:54:13 -0300

    fix unnecessary pointer

--- jwtmiddleware.go
+++ jwtmiddleware.go
@@ -236,7 +236,7 @@ func (m *JWTMiddleware) CheckJWT(next http.Handler) http.Handler {
 
 		// no err means we have a valid token, so set it into the
 		// context and continue onto next
-		*r = *r.Clone(context.WithValue(r.Context(), ContextKey{}, validToken))
+		r = r.Clone(context.WithValue(r.Context(), ContextKey{}, validToken))
 		next.ServeHTTP(w, r)
 	})
 }
