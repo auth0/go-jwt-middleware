65cecb87d4c4492fcef534f0a84c2bbb8eb8b8be
Sergiu Ghitea <sergiu.ghitea@auth0.com>
2021-10-27 17:39:04 +0200

    Update docs

--- error_handler.go
+++ error_handler.go
@@ -8,23 +8,26 @@ import (
 )
 
 var (
+	// ErrJWTMissing is returned when the JWT is missing.
 	ErrJWTMissing = errors.New("jwt missing")
+
+	// ErrJWTInvalid is returned when the JWT is invalid.
 	ErrJWTInvalid = errors.New("jwt invalid")
 )
 
 // ErrorHandler is a handler which is called when an error occurs in the
-// middleware. Among some general errors, this handler also determines the
-// response of the middleware when a token is not found or is invalid. The err
-// can be checked to be ErrJWTMissing or ErrJWTInvalid for specific cases. The
-// default handler will return a status code of 400 for ErrJWTMissing, 401 for
-// ErrJWTInvalid, and 500 for all other errors. If you implement your own
-// ErrorHandler you MUST take into consideration the error types as not
+// JWTMiddleware. Among some general errors, this handler also determines the
+// response of the JWTMiddleware when a token is not found or is invalid. The
+// err can be checked to be ErrJWTMissing or ErrJWTInvalid for specific cases.
+// The default handler will return a status code of 400 for ErrJWTMissing,
+// 401 for ErrJWTInvalid, and 500 for all other errors. If you implement your
+// own ErrorHandler you MUST take into consideration the error types as not
 // properly responding to them or having a poorly implemented handler could
-// result in the middleware not functioning as intended.
+// result in the JWTMiddleware not functioning as intended.
 type ErrorHandler func(w http.ResponseWriter, r *http.Request, err error)
 
 // DefaultErrorHandler is the default error handler implementation for the
-// middleware. If an error handler is not provided via the WithErrorHandler
+// JWTMiddleware. If an error handler is not provided via the WithErrorHandler
 // option this will be used.
 func DefaultErrorHandler(w http.ResponseWriter, r *http.Request, err error) {
 	w.Header().Set("Content-Type", "application/json")
@@ -42,9 +45,10 @@ func DefaultErrorHandler(w http.ResponseWriter, r *http.Request, err error) {
 	}
 }
 
-// invalidError handles wrapping a JWT validation error with the concrete error
-// ErrJWTInvalid. We do not expose this publicly because the interface methods
-// of Is and Unwrap should give the user all they need.
+// invalidError handles wrapping a JWT validation error with
+// the concrete error ErrJWTInvalid. We do not expose this
+// publicly because the interface methods of Is and Unwrap
+// should give the user all they need.
 type invalidError struct {
 	details error
 }
@@ -54,12 +58,13 @@ func (e *invalidError) Is(target error) bool {
 	return target == ErrJWTInvalid
 }
 
+// Error returns a string representation of the error.
 func (e *invalidError) Error() string {
 	return fmt.Sprintf("%s: %s", ErrJWTInvalid, e.details)
 }
 
-// Unwrap allows the error to support equality to the underlying error and not
-// just ErrJWTInvalid.
+// Unwrap allows the error to support equality to the
+// underlying error and not just ErrJWTInvalid.
 func (e *invalidError) Unwrap() error {
 	return e.details
 }
