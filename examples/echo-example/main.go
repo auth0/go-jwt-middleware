86dbbc751c4beaae105355764254d7082f0e7489
Mehul Gohil <mehulgohil75@gmail.com>
2023-06-29 23:07:53 +0530

    added echo example

---
+++ examples/echo-example/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	jwtmiddleware "github.com/auth0/go-jwt-middleware/v2"
+	"github.com/auth0/go-jwt-middleware/v2/validator"
+	"github.com/labstack/echo/v4"
+	"log"
+	"net/http"
+)
+
+// Try it out with:
+//
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnby1qd3QtbWlkZGxld2FyZS1leGFtcGxlIiwiYXVkIjoiYXVkaWVuY2UtZXhhbXBsZSIsInN1YiI6IjEyMzQ1Njc4OTAiLCJuYW1lIjoiSm9obiBEb2UiLCJpYXQiOjE1MTYyMzkwMjIsInVzZXJuYW1lIjoidXNlcjEyMyJ9.XFhrzWzntyINkgoRt2mb8dES84dJcuOoORdzKfwUX70
+//
+// which is signed with 'secret' and has the data:
+//
+//	{
+//	  "iss": "go-jwt-middleware-example",
+//	  "aud": "audience-example",
+//	  "sub": "1234567890",
+//	  "name": "John Doe",
+//	  "iat": 1516239022,
+//	  "username": "user123"
+//	}
+//
+// You can also try out the custom validation with:
+//
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnby1qd3QtbWlkZGxld2FyZS1leGFtcGxlIiwiYXVkIjoiYXVkaWVuY2UtZXhhbXBsZSIsInN1YiI6IjEyMzQ1Njc4OTAiLCJuYW1lIjoiSm9obiBEb2UiLCJpYXQiOjE1MTYyMzkwMjIsInVzZXJuYW1lIjoidXNlcjEyMyIsInNob3VsZFJlamVjdCI6dHJ1ZX0.Jf13PY_Oyu2x3Gx1JQ0jXRiWaCOb5T2RbKOrTPBNHJA
+//
+// which is signed with 'secret' and has the data:
+//
+//	{
+//	  "iss": "go-jwt-middleware-example",
+//	  "aud": "audience-example",
+//	  "sub": "1234567890",
+//	  "name": "John Doe",
+//	  "iat": 1516239022,
+//	  "username": "user123",
+//	  "shouldReject": true
+//	}
+
+func main() {
+	app := echo.New()
+
+	app.GET("/", func(ctx echo.Context) error {
+		claims, ok := ctx.Request().Context().Value(jwtmiddleware.ContextKey{}).(*validator.ValidatedClaims)
+		if !ok {
+			ctx.JSON(
+				http.StatusInternalServerError,
+				map[string]string{"message": "Failed to get validated JWT claims."},
+			)
+			return nil
+		}
+
+		customClaims, ok := claims.CustomClaims.(*CustomClaimsExample)
+		if !ok {
+			ctx.JSON(
+				http.StatusInternalServerError,
+				map[string]string{"message": "Failed to cast custom JWT claims to specific type."},
+			)
+			return nil
+		}
+
+		if len(customClaims.Username) != 0 {
+			ctx.JSON(
+				http.StatusBadRequest,
+				map[string]string{"message": "Username in JWT claims was empty."},
+			)
+			return nil
+		}
+
+		ctx.JSON(http.StatusOK, claims)
+		return nil
+	}, checkJWT)
+
+	log.Print("Server listening on http://localhost:3000")
+	err := app.Start(":3000")
+	if err != nil {
+		log.Fatalf("There was an error with the http server: %v", err)
+	}
+}
--- go.mod
+++ go.mod
@@ -10,7 +10,16 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/labstack/echo/v4 v4.10.2 // indirect
+	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	golang.org/x/crypto v0.4.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/crypto v0.6.0 // indirect
+	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
