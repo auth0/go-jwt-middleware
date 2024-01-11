1ee16f45d49d0bca2c2a0718f36b3fabc79bd861
bt-nia <74181389+bt-nia@users.noreply.github.com>
2024-01-11 16:41:22 +0000

    fix examples for v2.2.0

--- examples/http-jwks-example/main.go
+++ examples/http-jwks-example/main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/auth0/go-jwt-middleware/v2"
 	"github.com/auth0/go-jwt-middleware/v2/jwks"
 	"github.com/auth0/go-jwt-middleware/v2/validator"
+	jwtmiddleware "github.com/auth0/go-jwt-middleware/v2"
 )
 
 var handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
